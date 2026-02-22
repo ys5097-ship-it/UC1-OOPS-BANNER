46a8977f2b3ece8a3f95a949741d9c808a40c0c6
ys5097-ship-it <ys5097@srmist.edu.in>
2026-02-22 20:05:28 +0530

    Refactored banner using string.join()-UC3 implementation

---
+++ UC3.java
@@ -0,0 +1,55 @@
+public class UC1 {
+
+    public static void main(String[] args) {
+
+        String line1 = String.join(" ",
+                " ***** ",
+                " ***** ",
+                " ******",
+                " ***** ");
+
+        String line2 = String.join(" ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *");
+
+        String line3 = String.join(" ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *");
+
+        String line4 = String.join(" ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*     *");
+
+        String line5 = String.join(" ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "*     *");
+
+        String line6 = String.join(" ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "*     *");
+
+        String line7 = String.join(" ",
+                " ***** ",
+                " ***** ",
+                "*      ",
+                " ***** ");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+    }
+}
