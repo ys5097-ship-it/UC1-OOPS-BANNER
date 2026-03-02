c0f3366ca23f8cef113cb9149c9e8118e917daf6
ys5097-ship-it <ys5097@srmist.edu.in>
2026-03-02 09:09:48 +0530

    UC6 - Implemented static methods for O, P, S banner generation

--- UC1.java
+++ UC1.java
@@ -1,13 +1,52 @@
 public class UC1 {
+
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   ******   ***** ");
-        System.out.println("*     * *     *  *     * *     *");
-        System.out.println("*     * *     *  *     * *     *");
-        System.out.println("*     * *     *  ******  *     *");
-        System.out.println("*     * *     *  *       *     *");
-        System.out.println("*     * *     *  *       *     *");
-        System.out.println(" *****   *****   *        ***** ");
+        String[] o = getO();
+        String[] p = getP();
+        String[] s = getS();
 
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
+        }
+    }
+
+    // Method for O
+    public static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Method for P
+    public static String[] getP() {
+        return new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Method for S
+    public static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
     }
 }
